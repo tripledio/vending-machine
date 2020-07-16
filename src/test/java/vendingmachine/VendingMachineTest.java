5c49811a88422bebdf7b381dae0f65a68a1d64ae
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2020-07-16 21:51:21 +0200

    initial commit

---
+++ src/test/java/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,12 @@
+package vendingmachine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VendingMachineTest {
+
+    @Test
+    public void test1() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/test/kotlin/vendingmachine/VendingMachineKotlinTest.kt
@@ -0,0 +1,12 @@
+package vendingmachine
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class VendingMachineKotlinTest {
+
+    @Test
+    fun test1() {
+        assertTrue(true)
+    }
+}
